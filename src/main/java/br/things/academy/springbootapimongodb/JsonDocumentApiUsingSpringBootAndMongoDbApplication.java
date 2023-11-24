2910f9426c9c5fcd36a2be93f0b2806bac68e935
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-24 06:47:00 -0300

    add spring boot annotation to prevent data source auto config error

--- src/main/java/br/things/academy/springbootapimongodb/JsonDocumentApiUsingSpringBootAndMongoDbApplication.java
+++ src/main/java/br/things/academy/springbootapimongodb/JsonDocumentApiUsingSpringBootAndMongoDbApplication.java
@@ -3,8 +3,9 @@ package br.things.academy.springbootapimongodb;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class JsonDocumentApiUsingSpringBootAndMongoDbApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {

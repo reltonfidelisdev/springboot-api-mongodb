97ec71daceacb3af1731753b1eb33cbeefbc286f
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-24 06:07:04 -0300

    starting new project

---
+++ src/main/java/br/things/academy/springbootapimongodb/JsonDocumentApiUsingSpringBootAndMongoDbApplication.java
@@ -0,0 +1,13 @@
+package br.things.academy.springbootapimongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonDocumentApiUsingSpringBootAndMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonDocumentApiUsingSpringBootAndMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/things/academy/springbootapimongodb/JsonDocumentApiUsingSpringBootAndMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.things.academy.springbootapimongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonDocumentApiUsingSpringBootAndMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
